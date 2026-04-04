3f0fbe58a01568b404aad55c6605f16a3518fc12
Erik Ferrari <eferrari@datacolor.com>
2026-04-04 18:42:26 +0200

    Configure API chat

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
@@ -47,9 +48,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
